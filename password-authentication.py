f8b452b5513e6fd97f89bd086cfe0860db9f01e9
Vindhya Vodamodula <vindhya.a23@gmail.com>
2026-03-18 00:09:59 -0600

    Add Python program demonstrating loops, conditionals, and user input validation

---
+++ password-authentication.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  5 02:48:19 2025
+
+@author: vindhya
+#student ID: 3728392
+programe 3- Loops and if conditions
+
+"""
+
+while True:
+    #getting input from thr user
+    password=input("Enter password: ")
+    if password=="hello":
+          print("Welcome to the second half of the pregram!")
+          name=input("please enter your name: ")
+          if name=="vindhya":
+                print("What a great name!")
+                break
+          elif name=="Madonna" or name=="Cher":
+                 print("May I have your autograph, please?")
+                 break
+          else:
+                 print(name, ", that's a nice name.") 
+                 break
+    else:
+          print('Error!please enter valid password')
+          Ask=input("Do you want to try again?(yes/no): ")
+          if Ask=="yes":
+           continue
+          elif Ask=="no":
+            break
